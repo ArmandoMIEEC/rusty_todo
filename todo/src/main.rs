30b10b500e2e6f6e4b3c60c5374688ac4e8b859f
Armando <josearmandoborgesrodrigues@gmail.com>
2021-06-10 00:09:12 +0100

    refactored command parsing code

--- todo/src/main.rs
+++ todo/src/main.rs
@@ -1,5 +1,7 @@
 use std::env;
 
+const COMMANDS: [&'static str; 1] = ["help"];
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     let cmd: Command = Command::new(&args);
@@ -15,8 +17,13 @@ struct Command{
 impl Command{
     fn new(args: &[String]) -> Command{
         if args.len() < 1{
-            panic!("Not enough arguments! Find available commands: todo help");
+            panic!("todo: Not enough arguments! Find available commands: todo help");
         }
+        else if args.len() > 3{
+            panic!("todo: Too many arguments! Find available commands: todo help");
+        }
+
+        for cmd in
 
         let name: String = args[1].clone();
         let option: Option<String> = Some(args[2].clone());
